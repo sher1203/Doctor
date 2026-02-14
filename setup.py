ea727a730ebbc45bf1dc92dbecd33d9b493bda74
Kazi Ali <mubshali@gmail.com>
2026-02-14 18:53:18 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical chatbot",
+    version = "0.1.0",
+    author ="Kazi",
+    author_email="kazialijs@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
